f7a1e0f6c533444b8c978b2a134a671bc62e2c50
matheussantozqa <matheussantoz.qa@gmail.com>
2026-01-16 18:16:38 -0300

    Adjusted wdio.conf to generate browserstack tests repport

--- wdio.conf.js
+++ wdio.conf.js
@@ -9,6 +9,9 @@ export const config = {
 
   maxInstances: 1,
 
+  user: process.env.BROWSERSTACK_USERNAME,
+  key: process.env.BROWSERSTACK_ACCESS_KEY,
+
   capabilities: [{
     platformName: 'Android',
     'appium:deviceName': 'Google Pixel 5',
@@ -22,20 +25,26 @@ export const config = {
     }
   }],
 
-  user: process.env.BROWSERSTACK_USERNAME,
-  key: process.env.BROWSERSTACK_ACCESS_KEY,
+  services: [
+    ['browserstack', {
+      testObservability: true,
+      testObservabilityOptions: {
+        projectName: 'Appium BDD WDIO',
+        buildName: 'GitHub Actions Build',
+        buildTag: ['wdio', 'appium', 'android']
+      }
+    }]
+  ],
+
+  framework: 'cucumber',
+
+  reporters: ['spec'],
 
   logLevel: 'info',
   bail: 0,
   waitforTimeout: 10000,
   connectionRetryTimeout: 120000,
   connectionRetryCount: 1,
-
-  services: [],
-
-  framework: 'cucumber',
-
-  reporters: ['spec'],
 
   cucumberOpts: {
     require: ['./e2e/step-definitions/**/*.js'],
